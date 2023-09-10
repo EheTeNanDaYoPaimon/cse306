cfa5d684a77735d189cde9ee70d18e84c755dbd6
EheTeNanDaYoPaimon <98989799+EheTeNanDaYoPaimon@users.noreply.github.com>
2023-09-10 17:39:01 -0400

    wh

--- client.c
+++ client.c
@@ -18,6 +18,14 @@ int main(int argc, const char *argv[])
 
 	/* [C1]
 	 * Explaint the following here.
+	 
+	Variable sock will be set to the return value of socket(AF_INET, SOCK_STREAM, 0).
+	The function socket() will return -1 if error occure so then main() will print error message
+	and return -1 as well. However, if no error occure then socket() will return a file descriptor
+	and code will continue.
+
+	This part main just check if a connection is established or not.
+
 	 */
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
@@ -26,6 +34,15 @@ int main(int argc, const char *argv[])
 
 	/* [C2]
 	 * Explaint the following here.
+
+	Fill the amount of bytes needed for serv_addr with '0'. Then the struct serv_addr will have its
+	space for use. 
+
+	This is to avoid errors.
+
+	Then the serv_addr's variables will be set to AF_INET(ipv4 protocal)
+	and use htons() to get network byte order of port number.
+
 	 */
 	memset(&serv_addr, '0', sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
@@ -33,6 +50,11 @@ int main(int argc, const char *argv[])
 
 	/* [C3]
 	 * Explaint the following here.
+
+	Convert the ipv4 in serv_addr to binary form and check if succeeded.
+	It convert 127.0.0.1 to binary form and put it in the address of serv_addr.sin_addr
+	for future referencing.
+
 	 */
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
 		printf("\nInvalid address/ Address not supported \n");
@@ -41,6 +63,11 @@ int main(int argc, const char *argv[])
 
 	/* [C4]
 	 * Explaint the following here.
+
+	Start a connection with input as information stored in serv_addr struct.
+	Here sock which is the file descriptor is being used to determine the format of serv_addr.
+	If succeed then return 0 and code continue else -1 and print out error message and return.
+
 	 */
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
 		printf("\nConnection Failed \n");
@@ -50,18 +77,30 @@ int main(int argc, const char *argv[])
 
 	/* [C5]
 	 * Explaint the following here.
+
+	Ask the user to press a key and use getchar() to remove the user input.
+	Basically wait for the user to be ready and then do preceding steps.
+
 	 */
 	printf("Press any key to continue...\n");
 	getchar();
 
 	/* [C6]
 	 * Explaint the following here.
+
+	Send the string hello ("Hello from client") to the socket whick the file descriptor sock reference to.
+	Then print the messsage was sent.
+
 	 */
 	send(sock , hello , strlen(hello) , 0 );
 	printf("Hello message sent\n");
 
 	/* [C7]
 	 * Explaint the following here.
+
+	read the response after sending the message.
+	If error occur then read() will return -1 and stop running and print out error message
+	else read() will return the amount returned and continue. Then print out the message.
 	 */
 	if (read( sock , buffer, 1024) < 0) {
 		printf("\nRead Failed \n");
--- server.c
+++ server.c
@@ -19,6 +19,14 @@ int main(int argc, const char *argv[])
 
 	/* [S1]
 	 * Explaint the following here.
+
+	 Variable server_fd will be set to the return value of socket(AF_INET, SOCK_STREAM, 0).
+	The function socket() will return -1 if error occure so then main() will print error message
+	and return -1 as well. However, if no error occure then socket() will return a file descriptor
+	and code will continue.
+
+	This part main just check if a connection is established or not.
+
 	 */
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("socket failed");
@@ -27,6 +35,13 @@ int main(int argc, const char *argv[])
 
 	/* [S2]
 	 * Explaint the following here.
+
+	setsockopt manipulates options in socket referenced by server_fd.
+	Since SOL_SOCKET is set then this function call will try to manipulate
+	the options at socket api level. The option name is the third parameter
+	and will be set to fourth parameter.
+
+	If there is error then it will exit.
 	 */
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
 		       &opt, sizeof(opt))) {
@@ -36,6 +51,10 @@ int main(int argc, const char *argv[])
 
 	/* [S3]
 	 * Explaint the following here.
+
+	 Sets the value of the variables inside struct address.
+	 The sin_port just like the client side, will be use 
+	 htons() to get network byte order of port number.
 	 */
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
@@ -43,6 +62,10 @@ int main(int argc, const char *argv[])
 
 	/* [S4]
 	 * Explaint the following here.
+
+	After setting up address struct variable it will then be 
+	used and bind to the server_fd which is the file descriptor.
+
 	 */
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
 		perror("bind failed");
@@ -51,6 +74,11 @@ int main(int argc, const char *argv[])
 
 	/* [S5]
 	 * Explaint the following here.
+
+	listen for connection on the socket which server_fd reference to.
+	3 is the queue for which the pending connection for server_fd may grow.
+	So there can only be 3 connection request be pended.
+
 	 */
 	if (listen(server_fd, 3) < 0) {
 		perror("listen");
@@ -59,6 +87,12 @@ int main(int argc, const char *argv[])
 
 	/* [S6]
 	 * Explaint the following here.
+
+	start to accept the first connection request and new_socket will have the new file 
+	descriptor for the accepted socket. 
+
+	if succeed then continue else exit
+
 	 */
 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
 				 (socklen_t*)&addrlen)) < 0) {
@@ -68,12 +102,22 @@ int main(int argc, const char *argv[])
 
 	/* [S7]
 	 * Explaint the following here.
+
+	Ask the user to press a key and use getchar() to remove the user input.
+	Basically wait for the user to be ready and then do preceding steps.
+
+
 	 */
 	printf("Press any key to continue...\n");
 	getchar();
 
 	/* [S8]
 	 * Explaint the following here.
+
+	read the response from client. (the client side sends a hello message)
+	If error occur then read() will return -1 and stop running and print out error message
+	else read() will return the amount returned and continue. Then print out the message.
+	
 	 */
 	if (read( new_socket , buffer, 1024) < 0) {
 		perror("accept");
@@ -83,6 +127,11 @@ int main(int argc, const char *argv[])
 
 	/* [S9]
 	 * Explaint the following here.
+
+	(send reponse from the server side)
+	Send the string hello ("Hello from client") to the socket whick the file descriptor sock reference to.
+	Then print the messsage was sent.
+
 	 */
 	send(new_socket , hello , strlen(hello) , 0 );
 	printf("Hello message sent\n");
